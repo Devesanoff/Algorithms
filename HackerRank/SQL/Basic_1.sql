1e3f9cff79a421c30afe7836f230f2b29e0ffd28
Devesanoff <esanovmadaminjon@gmail.com>
2026-06-20 21:06:42 +0500

    bismillah with hackerrank

---
+++ HackerRank/SQL/Basic_1.sql
@@ -0,0 +1,9 @@
+8
+
+-- ha faqat shu javob.
+--You are given two sets.
+--Set A = {1,2,3,4,5,6}
+--Set B = {2,3,4,5,6,7,8}
+
+--How many elements are present in ?
+--Only enter the correct integer in the editor below. Do not include any extra spaces, tabs or newlines. 
